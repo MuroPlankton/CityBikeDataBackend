26b32c9a9665fe7a21c25ca24aa0542158e19adc
Miro Planting <73102721+MuroPlankton@users.noreply.github.com>
2022-07-02 18:12:45 +0300

    Initialized project with Spring Initializr

---
+++ CityBikeDataBackend/src/main/java/com/miroplanting/CityBikeDataBackend/CityBikeDataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.miroplanting.CityBikeDataBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityBikeDataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityBikeDataBackendApplication.class, args);
+	}
+
+}
---
+++ CityBikeDataBackend/src/test/java/com/miroplanting/CityBikeDataBackend/CityBikeDataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miroplanting.CityBikeDataBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityBikeDataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
